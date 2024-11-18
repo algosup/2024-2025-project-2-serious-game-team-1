c9da8b9e8e4ad93ec6bac5df682ffa25ddbbb1ca
SANTOS Alexis <alexis.santos@algosup.com>
2024-11-18 10:00:43 +0100

    Update tried to fix inventory

--- theworld/scripts/inventory_gestion.gd
+++ theworld/scripts/inventory_gestion.gd
@@ -1,68 +1,85 @@
 extends Control
 
-@onready var icon: TextureRect = get_node("Inventory/Description/DescriptionBox/Header/Icon")
-@onready var description_label: RichTextLabel = get_node("Inventory/Description/DescriptionBox/DescriptionLabel")
-@onready var name_label: Label = get_node("Inventory/Description/DescriptionBox/Header/Name_Label")
+@onready var icon: TextureRect = $Inventory/Description/DescriptionBox/Header/Icon
+@onready var description_label: RichTextLabel = $Inventory/Description/DescriptionBox/DescriptionLabel
+@onready var name_label: Label = $Inventory/Description/DescriptionBox/Header/Name_Label
 
 @warning_ignore("shadowed_global_identifier")
 const Item = preload("res://scripts/item_class.gd")
 
 var inventory : Array = [
-	preload("res://assets/items/bin_bag.tres"), 
-	preload("res://assets/items/extinguisher.tres"), 
-	preload("res://assets/items/planks.tres"), 
-	preload("res://assets/items/shovel.tres"), 
-	preload("res://assets/items/tree_shoots.tres")
-	]
+	preload("res://assets/items/bin_bag.tres"),
+	preload("res://assets/items/extinguisher.tres"),
+	preload("res://assets/items/planks.tres"),
+	preload("res://assets/items/shovel.tres"),
+	preload("res://assets/items/tree_shoots.tres"), 
+	null, null, null, null, null,
+	null, null, null, null, null,
+	null, null, null, null, null
+]
+
+@onready var inventory_slot_1: PanelContainer = $Inventory/GridInventory/inventory_slot1
+@onready var inventory_slot_2: PanelContainer = $Inventory/GridInventory/inventory_slot2
+@onready var inventory_slot_3: PanelContainer = $Inventory/GridInventory/inventory_slot3
+@onready var inventory_slot_4: PanelContainer = $Inventory/GridInventory/inventory_slot4
+@onready var inventory_slot_5: PanelContainer = $Inventory/GridInventory/inventory_slot5
+@onready var inventory_slot_6: PanelContainer = $Inventory/GridInventory/inventory_slot6
+@onready var inventory_slot_7: PanelContainer = $Inventory/GridInventory/inventory_slot7
+@onready var inventory_slot_8: PanelContainer = $Inventory/GridInventory/inventory_slot8
+@onready var inventory_slot_9: PanelContainer = $Inventory/GridInventory/inventory_slot9
+@onready var inventory_slot_10: PanelContainer = $Inventory/GridInventory/inventory_slot10
+@onready var inventory_slot_11: PanelContainer = $Inventory/GridInventory/inventory_slot11
+@onready var inventory_slot_12: PanelContainer = $Inventory/GridInventory/inventory_slot12
+@onready var inventory_slot_13: PanelContainer = $Inventory/GridInventory/inventory_slot13
+@onready var inventory_slot_14: PanelContainer = $Inventory/GridInventory/inventory_slot14
+@onready var inventory_slot_15: PanelContainer = $Inventory/GridInventory/inventory_slot15
+@onready var inventory_slot_16: PanelContainer = $Inventory/GridInventory/inventory_slot16
+@onready var inventory_slot_17: PanelContainer = $Inventory/GridInventory/inventory_slot17
+@onready var inventory_slot_18: PanelContainer = $Inventory/GridInventory/inventory_slot18
+@onready var inventory_slot_19: PanelContainer = $Inventory/GridInventory/inventory_slot19
+@onready var inventory_slot_20: PanelContainer = $Inventory/GridInventory/inventory_slot20
 
 
 
 var slots : Array[PanelContainer] = [
-	$Inventory/GridInventory/slot0, 
-	get_node("Inventory/GridInventory/slot1"),
-	get_node("Inventory/GridInventory/slot2"), 
-	get_node("Inventory/GridInventory/slot3"), 
-	get_node("Inventory/GridInventory/slot4"), 
-	get_node("Inventory/GridInventory/slot5"), 
-	get_node("Inventory/GridInventory/slot6"), 
-	get_node("Inventory/GridInventory/slot7"), 
-	get_node("Inventory/GridInventory/slot8"), 
-	get_node("Inventory/GridInventory/slot9"), 
-	get_node("Inventory/GridInventory/slot10"), 
-	get_node("Inventory/GridInventory/slot11"), 
-	get_node("Inventory/GridInventory/slot12"), 
-	get_node("Inventory/GridInventory/slot13"), 
-	get_node("Inventory/GridInventory/slot14"), 
-	get_node("Inventory/GridInventory/slot15"), 
-	get_node("Inventory/GridInventory/slot16"), 
-	get_node("Inventory/GridInventory/slot17"), 
-	get_node("Inventory/GridInventory/slot18"), 
-	get_node("Inventory/GridInventory/slot19")
+	inventory_slot_1, inventory_slot_2, inventory_slot_3, inventory_slot_4, inventory_slot_5, 
+	inventory_slot_6, inventory_slot_7, inventory_slot_8, inventory_slot_9, inventory_slot_10,
+	inventory_slot_11, inventory_slot_12, inventory_slot_13, inventory_slot_14, inventory_slot_15,
+	inventory_slot_16, inventory_slot_17, inventory_slot_18, inventory_slot_19, inventory_slot_20
 	]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#for i in range(inventory.size()):
-			#if i < slots.size() and i <= inventory.size()-1:
-				#slots[i].set_item(inventory[i])
-	for node in slots:
-		print(node)
 
-var item_in_hand: Item = null #Credit Matteo 
-		
+# Called when the node enters the scene tree for the first time.
+func _enter_tree() -> void:
+	for i in range(inventory.size()):
+		if slots[i] != null:
+			slots[i].set_item(inventory[i])
+		else:
+			print("Slot %d is Nil" % i)
+
+	# Vérifiez les chemins des nœuds
+	for i in range(1, slots.size()+1):
+		var slot_path = "Inventory/GridInventory/inventory_slot%d" % i
+		var slot_node = get_node(slot_path)
+		if slot_node != null:
+			print("Node found: %s" % slot_path)
+		else:
+			print("Node not found: %s" % slot_path)
+
+var item_in_hand: Item = null #Credit Matteo
 
 func add_to_inventory(item : Item):
 	#Credit Matteo & Alexis
 	if item not in inventory:
-		if inventory.size() == slots.size():
-			print("Inventory is full")
-		else:
-			inventory.append(item)
-			print("Added %d to inventory" % item.title)
-			item.pick_up()
+		for i in inventory:
+			if i == null :
+				inventory[i] = item
+				print("Added %s to inventory" % item.title)
+				item.pick_up()
+				break
+		print("Inventory is full")
 	else:
-		print("%d is already in inventory" % item.title)
-
+		print("%s is already in inventory" % item.title)
 
 func _process(_delta: float) -> void:
 	if Variable.InventoryOpen:
@@ -71,7 +88,6 @@ func _process(_delta: float) -> void:
 			Variable.CaptureMouseOff()
 		if !Variable.PlayerIsPaused:
 			Variable.pause_character()
-		
 	else:
 		get_node(".").hide()
 		if not Variable.MouseCapture:
@@ -83,11 +99,9 @@ func OpenInventory():
 	print("Open Inventory")
 	Variable.InventoryOpen = !Variable.InventoryOpen
 
-
 func CloseInventory():
 	print("Close Inventory")
 	Variable.InventoryOpen = !Variable.InventoryOpen
-
 
 func set_description(item : Item):
 	if name_label != null:
@@ -102,4 +116,3 @@ func set_description(item : Item):
 		description_label.text = item.description
 	else:
 		print("null description_label")
-	
