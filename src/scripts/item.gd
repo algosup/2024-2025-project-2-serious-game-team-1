7b2e006322bb38bde644de493023a1d504fe9428
Guillaume Despaux <146001117+GuillaumeDespaux@users.noreply.github.com>
2024-11-24 17:48:25 +0100

    Only Map

--- src/scripts/item.gd
+++ src/scripts/item.gd
@@ -6,13 +6,12 @@ class_name Item
 
 @export var item_name: String = "Default Item"
 
-#var res = Alldictionary.recup_item(item_name)
 
 @onready var rigidbody = self  # Reference to the RigidBody3D itself (assuming this script is attached to a RigidBody3D)
 
 # Called when picked up
 func pick_up():
 	print(self.name + " picked up")
-	Alldictionary.add_item(item_name)
+	EcoFollow.add_to_inventory(self)
 	queue_free()
 	
