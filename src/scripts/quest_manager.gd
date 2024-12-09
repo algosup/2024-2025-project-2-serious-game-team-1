1cf7b7d8637b7d49eaa9ddb322f1f07f4dc6c420
Alexis SANTOS <alexis.santos@algosup.com>
2024-12-09 14:05:13 +0100

    add quest goal function

--- src/scripts/quest_manager.gd
+++ src/scripts/quest_manager.gd
@@ -2,7 +2,12 @@ extends Node
 
 var tutorial_quest = preload("res://assets/quests/tutorial.tres")
 
-
 func _process(_delta) -> void:
 	if tutorial_quest in AllDictionary.active_quests:
 		pass
+
+func goal_text(quest : Quests_Resources):
+	if quest.quests_goal_objective == 0:
+		return quest.quests_goal_label_part1
+	else:
+		return quest.quests_goal_label_part1 + " " + str(quest.quests_goal_counter) + "/" + str(quest.quests_goal_objective) + " " +quest.quests_goal_label_part2
--- src/scripts/quests_panel_slot.gd
+++ src/scripts/quests_panel_slot.gd
@@ -2,7 +2,8 @@ extends Panel
 
 @export var quests : Quests_Resources
 
-@onready var quest_label: Label = $quest_label
+@onready var quest_label: Label = $VBoxContainer/quest_label
+@onready var goal_label: Label = $VBoxContainer/goal_label
 
 @onready var quests_panel_slot: Panel = $"."
 @onready var quests_panel: Control = $"../../.."
@@ -11,8 +12,9 @@ extends Panel
 func _process(_delta):
 	if quests != null:
 		quest_label.text = quests.quests_name
-		var vbox_size = Vector2(quests_panel_slot.get_size().x, quest_label.get_size().y + 20)
+		goal_label = QuestManager.goal_text(quests)
+		var vbox_size = Vector2(quests_panel_slot.get_size().x, quest_label.get_size().y + goal_label.get_size().y + 20)
 		quests_panel_slot.set_size(vbox_size)
-		quests_panel.set_panel_size()
 	else:
 		quest_label.text = "Empty Quest Slot"
+		
