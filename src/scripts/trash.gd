6da9c21776c4808903037d28dbf6077b5ae6bbef
Alexis SANTOS <alexis.santos@algosup.com>
2024-12-09 16:41:23 +0100

    try another methode for trash counter

--- src/scripts/trash.gd
+++ src/scripts/trash.gd
@@ -8,7 +8,7 @@ var bin_bag = preload("res://assets/items/bin_bag.tres")
 var clean_beach_quests = preload("res://assets/quests/clean_beach.tres")
 
 func cleanup():
-	var quest_id : int
+	var quest
 	# Print the inventory contents for debugging
 	print("Inventory contents:", AllDictionary.inventory)
 	# Check if "bin bag" exists in the autoload inventory
@@ -16,8 +16,10 @@ func cleanup():
 		print("Cleaning up", waste_name)
 		EcoFollow.set_eco_beach(10)
 		print(EcoFollow.eco_beach)
-		if clean_beach_quests in AllDictionary.active_quests:
-			clean_beach_quests.add_counter()
+		if AllDictionary.active_quests.has(clean_beach_quests):
+			for i in range(len(AllDictionary.active_quests)):
+				quest = AllDictionary.active_quests[i]
+			quest.add_counter()
 		queue_free()  # Remove the waste item from the scene
 	else:
 		print(no_bin_bag_message)  # Print message if bin bag is missing
