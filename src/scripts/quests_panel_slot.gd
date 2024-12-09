95a24c51f81011c5aaf46f211d760446542cdc8e
Alexis SANTOS <alexis.santos@algosup.com>
2024-12-09 16:09:11 +0100

    try another things for waste counter

--- src/scripts/quests_panel_slot.gd
+++ src/scripts/quests_panel_slot.gd
@@ -12,7 +12,7 @@ extends Panel
 func _process(_delta):
 	if quests != null:
 		quest_label.text = quests.quests_name
-		goal_label = QuestManager.goal_text(quests)
+		goal_label.text = QuestManager.goal_text(quests)
 		var vbox_size = Vector2(quests_panel_slot.get_size().x, quest_label.get_size().y + goal_label.get_size().y + 20)
 		quests_panel_slot.set_size(vbox_size)
 	else:
--- src/scripts/trash.gd
+++ src/scripts/trash.gd
@@ -8,6 +8,7 @@ var bin_bag = preload("res://assets/items/bin_bag.tres")
 var clean_beach_quests = preload("res://assets/quests/clean_beach.tres")
 
 func cleanup():
+	var quest_id : int
 	# Print the inventory contents for debugging
 	print("Inventory contents:", AllDictionary.inventory)
 	# Check if "bin bag" exists in the autoload inventory
@@ -15,9 +16,11 @@ func cleanup():
 		print("Cleaning up", waste_name)
 		EcoFollow.set_eco_beach(10)
 		print(EcoFollow.eco_beach)
-		if clean_beach_quests in AllDictionary.active_quests:
-			if clean_beach_quests.quests_goal_objective != clean_beach_quests.quests_goal_counter: 
-				clean_beach_quests.quests_goal_counter += 1
+		for i in range(len(AllDictionary.active_quests)):
+			if AllDictionary.active_quests[i] == clean_beach_quests:
+				quest_id = i
+		if AllDictionary.active_quests[quest_id].quests_goal_objective != AllDictionary.active_quests[quest_id].quests_goal_counter: 
+				AllDictionary.active_quests[quest_id].quests_goal_counter += 1
 		queue_free()  # Remove the waste item from the scene
 	else:
 		print(no_bin_bag_message)  # Print message if bin bag is missing
