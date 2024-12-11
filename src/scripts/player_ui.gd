1fcfa62585e96235a55fb2e3591146f870b80bc3
Alexis SANTOS <alexis.santos@algosup.com>
2024-12-11 10:11:57 +0100

    fix quests panel issues and test zone label issues

--- src/scripts/player_ui.gd
+++ src/scripts/player_ui.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var progress_bar: ProgressBar = $CanvasLayer/ProgressBar
 
 func _process(_delta):
-	location_label.text = str(Zones.zone_name)
+	location_label.text = Zones.zone_name
 	progress_bar.value = EcoFollow.eco_town
 
 func Player_uiVisible():
