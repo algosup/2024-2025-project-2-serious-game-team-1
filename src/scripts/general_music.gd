17dfd4b75f784f6cefc16e121bd3717dd5cac3c3
Guillaume Despaux <146001117+GuillaumeDespaux@users.noreply.github.com>
2024-12-13 13:47:01 +0100

    Main Menu Music Adding

--- src/scripts/general_music.gd
+++ src/scripts/general_music.gd
@@ -1,9 +1,11 @@
 extends AudioStreamPlayer
 
+var menu_music = preload("res://assets/music/instrumental-ambient-calming-269887.mp3")
 var tracks = [preload("res://assets/music/Enchanted Valley.mp3"), preload("res://assets/music/Morning.mp3")]
 var index = 0
 
 @onready var streamPlayed: AudioStreamPlayer = $"."
+@onready var streamMenu: AudioStreamPlayer = $"."
 
 func playMusic(index):
 	streamPlayed.stream = tracks[index]
@@ -26,3 +28,6 @@ func _on_introduction_animation_cinematic_done() -> void:
 	playMusic(index)
 	print("intro done =0? " + str(index))
 	
+	
+func _on_play_button_pressed() -> void:
+	streamMenu.stream_paused = true
