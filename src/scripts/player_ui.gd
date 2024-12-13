bd24083d3d48ed3181f95a614f1c780669624d45
Alexis Santos <alexis.santos@algosup.com>
2024-12-13 14:48:45 +0100

    remove cursor in documentary menu
    Co-Authored-By: Matteo LEFIN <146164921+Mattstar64@users.noreply.github.com>

--- src/scripts/player_ui.gd
+++ src/scripts/player_ui.gd
@@ -1,5 +1,13 @@
 extends Control
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
+func _process(_delta):
+	if Input.is_action_just_pressed("documentary"):
+		canvas_layer.visible = !canvas_layer.visible
+	if Input.is_action_just_pressed("escape"):
+		if Variable.DocumentaryMenuOpen:
+			canvas_layer.visible = true
 
 func Player_uiVisible():
 	Variable.player_uiVisible = true
