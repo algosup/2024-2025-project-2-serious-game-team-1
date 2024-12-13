8aca844c3e886a20fbb7ae496a000ca92b4c2d33
Matteo LEFIN <146164921+Mattstar64@users.noreply.github.com>
2024-12-13 09:44:47 +0100

    add player ui modifications by zones

--- src/scripts/eco_follow.gd
+++ src/scripts/eco_follow.gd
@@ -1,9 +1,11 @@
 extends Node
 
-var eco_town: float = 35
+var eco_town: float = 0
 var eco_beach: float = 0
 var eco_forest: float = 0
 var eco_farm:float = 0
+var eco_port:float = 0
+var eco_mountains: float = 0
 var inventory: Array = []
 
 
@@ -11,8 +13,11 @@ var Localisation: Dictionary = {
 	"town" : eco_town,
 	"beach" : eco_beach,
 	"forest" : eco_forest,
-	"farm" : eco_farm
+	"farm" : eco_farm,
+	"port" : eco_port,
+	"mountains": eco_mountains,
 }
+
 func set_eco_city(value):
 	eco_town = clamp(value, 0, 100)
 func set_eco_beach(value):
@@ -21,3 +26,20 @@ func set_eco_forest(value):
 	eco_forest = clamp(value, 0, 100)
 func set_eco_farm(value):
 	eco_farm = clamp(value, 0, 100)
+
+func eco_zone(zone: String, value: float):
+	if zone == "Town":
+		value = eco_town
+	if zone == "Beach":
+		value = eco_beach
+	if zone == "Farm":
+		value = eco_farm
+	if zone == "Forest":
+		value = eco_forest
+	if zone == "Port":
+		value = eco_port
+	if zone == "Mountains":
+		value = eco_mountains
+	
+	return value
+		
--- src/scripts/player_ui.gd
+++ src/scripts/player_ui.gd
@@ -1,11 +1,5 @@
 extends Control
 
-@onready var location_label: Label = %Location_Label
-@onready var progress_bar: ProgressBar = $CanvasLayer/ProgressBar
-
-func _process(_delta):
-	location_label.text = Zones.zone_name
-	progress_bar.value = EcoFollow.eco_town
 
 func Player_uiVisible():
 	Variable.player_uiVisible = true
